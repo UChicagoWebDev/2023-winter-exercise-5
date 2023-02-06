b5c1a089699c2b411190a6fbce0d37d6828e039a
Trevor Austin <trevor@scenery.video>
2023-02-06 02:15:28 -0600

    Re-work to remove requirements not covered in Week 5

---
+++ app.py
@@ -0,0 +1,169 @@
+import logging
+import string
+import traceback
+import random
+import sqlite3
+from datetime import datetime
+from flask import * # Flask, g, redirect, render_template, request, url_for
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+def get_db():
+    db = getattr(g, '_database', None)
+
+    if db is None:
+        db = g._database = sqlite3.connect('db/watchparty.sqlite3')
+        db.row_factory = sqlite3.Row
+        setattr(g, '_database', db)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    db = get_db()
+    cursor = db.execute(query, args)
+    print("query_db")
+    print(cursor)
+    rows = cursor.fetchall()
+    print(rows)
+    db.commit()
+    cursor.close()
+    if rows:
+        if one: 
+            return rows[0]
+        return rows
+    return None
+
+def new_user():
+    name = "Unnamed User #" + ''.join(random.choices(string.digits, k=6))
+    password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+    api_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
+    u = query_db('insert into users (name, password, api_key) ' + 
+        'values (?, ?, ?) returning id, name, password, api_key',
+        (name, password, api_key),
+        one=True)
+    return u
+
+def get_user_from_cookie(request):
+    user_id = request.cookies.get('user_id')
+    password = request.cookies.get('user_password')
+    if user_id and password:
+        return query_db('select * from users where id = ? and password = ?', [user_id, password], one=True)
+    return None
+
+def render_with_error_handling(template, **kwargs):
+    try:
+        return render_template(template, **kwargs)
+    except:
+        t = traceback.format_exc()
+        return render_template('error.html', args={"trace": t}), 500
+
+# ------------------------------ NORMAL PAGE ROUTES ----------------------------------
+
+@app.route('/')
+def index():
+    print("index") # For debugging
+    user = get_user_from_cookie(request)
+
+    if user:
+        rooms = query_db('select * from rooms')
+        return render_with_error_handling('index.html', user=user, rooms=rooms)
+    
+    return render_with_error_handling('index.html', user=None, rooms=None)
+
+@app.route('/rooms/new', methods=['GET', 'POST'])
+def create_room():
+    print("create room")
+    user = get_user_from_cookie(request)
+    if user is None: return {}, 403
+
+    if (request.method == 'POST'):
+        name = "Unnamed Room " + ''.join(random.choices(string.digits, k=6))
+        room = query_db('insert into rooms (name) values (?) returning id', [name], one=True)            
+        return redirect(f'{room["id"]}')
+    else:
+        return app.send_static_file('create_room.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print("signup")
+    user = get_user_from_cookie(request)
+
+    if user:
+        return render_with_error_handling('signup.html', user=user) # redirect('/')
+    
+    if request.method == 'POST':
+        u = new_user()
+        print("u")
+        print(u)
+        for key in u.keys():
+            print(f'{key}: {u[key]}')
+
+        resp = make_response(render_with_error_handling('signup.html', user=u))
+        resp.set_cookie('user_id', str(u['id']))
+        resp.set_cookie('user_password', u['password'])
+        return resp
+    
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print("login")
+    user = get_user_from_cookie(request)
+
+    if user:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['name']
+        u = query_db('select * from users where name = ? and password = ?', [name, password], one=True)
+        if user:
+            resp = make_response(redirect("/"))
+            resp.set_cookie('user_id', u.id)
+            resp.set_cookie('user_password', u.password)
+            return resp
+
+    return render_with_error_handling('login.html', failed=True)   
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect('/'))
+    resp.set_cookie('user_id', '')
+    resp.set_cookie('user_password', '')
+    return resp
+
+@app.route('/rooms/<int:room_id>')
+def room(room_id):
+    user = get_user_from_cookie(request)
+    if user is None: return redirect('/')
+
+    room = query_db('select * from rooms where id = ?', [room_id], one=True)
+    return render_with_error_handling('room.html',
+            room=room, user=user)
+
+# -------------------------------- API ROUTES ----------------------------------
+
+# POST to change the user's name
+
+# POST to change the user's password
+
+# POST to change the name of a room
+
+# GET to get all the messages in a room
+
+# POST to post a new message to a room
+
+# Change username
+
+# Change room name
+
+# Get messages in a room
+
+# Post a message in a room
---
+++ static/script.js
@@ -0,0 +1,24 @@
+/* For index.html */
+
+// TODO: If a user clicks to create a chat, create an auth key for them
+// and save it. Redirect the user to /chat/<chat_id>
+function createChat() {
+
+}
+
+/* For chat.html */
+
+// TODO: Fetch the list of existing chat messages.
+// POST to the API when the user posts a new message.
+// Automatically poll for new messages on a regular interval.
+function postMessage() {
+  return;
+}
+
+function getMessages() {
+  return;
+}
+
+function startMessagePolling() {
+  return;
+}
